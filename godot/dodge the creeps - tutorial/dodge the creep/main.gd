17f4602fdc109c7a176f0e5726a2d423151f54f2
Viswadeep Sarangi <deepun.deep@gmail.com>
2024-02-07 23:56:18 +0000

    dodge the creep - completed!

--- godot/dodge the creeps - tutorial/dodge the creep/main.gd
+++ godot/dodge the creeps - tutorial/dodge the creep/main.gd
@@ -3,29 +3,33 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	new_game()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#new_game()
+#
 
 
 func game_over():
+	$Music.stop()
+	$GameOverSound.play()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	
 	
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
+	$HUD.update_score(score)
+	$Music.play()
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 
 
 func _on_score_timer_timeout():
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
