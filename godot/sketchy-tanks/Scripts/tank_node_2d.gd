8f4065c1fcd60f40c6d668f1c1ff55d90ec385a2
Viswadeep Sarangi <viswadeep.sarangi@gmail.com>
2024-12-27 01:32:27 +0530

    good start already. created most assets. testing out tank functionality now

---
+++ godot/sketchy-tanks/Scripts/tank_node_2d.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func check_input_controls():
+	if Input.is_action_pressed('move_left'):
+		pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
