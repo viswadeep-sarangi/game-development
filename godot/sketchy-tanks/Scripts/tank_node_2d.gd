a5d7a4e8e62558473528ed45f2773ee8a75d4fbc
Viswadeep Sarangi <deepun.deep@gmail.com>
2025-01-21 05:58:09 +0000

    added proper tank movement

--- godot/sketchy-tanks/Scripts/tank_node_2d.gd
+++ godot/sketchy-tanks/Scripts/tank_node_2d.gd
@@ -29,26 +29,25 @@ func check_firing_controls():
 func _process(_delta: float) -> void:
 	check_firing_controls()
 
-func _physics_process(delta):
+func _physics_process(delta):	
 	# Reset velocity
 	velocity = Vector2.ZERO
 	is_turning_direction = 0
 	# Handle rotation
 	if Input.is_action_pressed("move_left"):
-		rotation_degrees -= rotation_speed * delta
-		is_turning_direction = -1
+		rotation_degrees = -90
+		velocity.x = -move_speed
 	elif Input.is_action_pressed("move_right"):
-		rotation_degrees += rotation_speed * delta
-		is_turning_direction = 1
-
-	# Handle forward/backward movement
-	if Input.is_action_pressed("move_up"):
-		# Move in the direction the tank is facing
-		velocity = Vector2.UP.rotated(rotation).normalized() * move_speed
+		rotation_degrees = 90
+		velocity.x = move_speed
+	elif Input.is_action_pressed("move_up"):
+		rotation_degrees = 0
+		velocity.y = -move_speed
 	elif Input.is_action_pressed("move_down"):
-		# Move backward in the direction the tank is facing
-		velocity = Vector2.UP.rotated(rotation).normalized() * -move_speed
-
+		rotation_degrees = 180
+		velocity.y = move_speed
+		
+	#velocity = velocity.normalized()
 	# Apply movement
 	move_and_collide(velocity)
 	animation_helper.update_velocity(velocity, is_turning_direction)
