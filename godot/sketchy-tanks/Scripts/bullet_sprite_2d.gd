fd125737a356c810642b3c049fbc3de354e1c8cf
Viswadeep Sarangi <viswadeep.sarangi@gmail.com>
2025-02-05 21:55:54 +0000

    added bullet collision anim

--- godot/sketchy-tanks/Scripts/bullet_sprite_2d.gd
+++ godot/sketchy-tanks/Scripts/bullet_sprite_2d.gd
@@ -1,10 +1,12 @@
 extends RigidBody2D
 @export var bullet_speed: float = 500.0 
+@export var implosion_anim:PackedScene
+@onready var bullet_sprite:Sprite2D = $BulletSprite2D
+@onready var collision_shape:CollisionShape2D = $CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	linear_velocity = Vector2.RIGHT.rotated(rotation) * bullet_speed
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -14,3 +16,9 @@ func _process(_delta: float) -> void:
 func modulate_bullet_color(c:Color):
 	var sprite = $BulletSprite2D
 	sprite.modulate = c
+
+func _on_body_entered(body: Node) -> void:
+	var boom:Node = implosion_anim.instantiate()
+	boom.global_position = global_position
+	get_parent().add_child(boom)
+	queue_free()
